bfd4da646fe759d7d01d37ad6b63ec8d9b07ea8a
maelpseudo <mael.cuny@gmail.com>
2024-01-03 23:53:13 +0100

    Modified monty.h, push.c, pall.c

---
+++ pall.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * _pall - prints all the elements of the stack
+ * @stack: list to print its elements
+ * @line_number: line number
+ * Return: Nothing
+ */
+
+void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+stack_t *current = *stack;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
